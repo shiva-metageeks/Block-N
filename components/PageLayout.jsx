751a64e0c8f4eb962560dfd68f898a36e5f83913
shiva-metageeks <shiva@metageeks.tech>
2024-07-03 14:13:38 +0530

    accept cookie conent added

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -7,6 +7,8 @@ import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 import GetInTouch from './GetInTouch'
 import ThemeSwitcher from './ThemeSwitcher'
+import 'react-hook-consent/dist/styles/style.css'
+import { ConsentBanner, ConsentProvider } from 'react-hook-consent'
 
 export default function PageLayout({ children }) {
   const pathname = usePathname()
@@ -25,10 +27,39 @@ export default function PageLayout({ children }) {
           {/* <ThemeSwitcher>
             
           </ThemeSwitcher> */}
-          <Navbar setPopup={setPopup} />
-          <main>{children}</main>
-          {popup ? <GetInTouch setPopup={setPopup} /> : null}
-          <Footer />
+          <ConsentProvider
+            options={{
+              services: [
+                {
+                  id: 'myid',
+                  name: 'MyName',
+                  scripts: [
+                    { id: 'external-script', src: 'https://myscript.com/script.js' },
+                    // { id: 'inline-code', code: `alert("I am a JavaScript code");` },
+                  ],
+                  cookies: [{ pattern: 'cookie-name' }, { pattern: /regex/ }],
+                  localStorage: ['local-storage-key'],
+                  sessionStorage: ['session-storage-key'],
+                  mandatory: true,
+                },
+              ],
+              // customHash: 'my-custom-hash', // optional, e.g. when changing the options based on language
+              theme: 'light',
+            }}>
+            <Navbar setPopup={setPopup} />
+            <main>{children}</main>
+            {popup ? <GetInTouch setPopup={setPopup} /> : null}
+            <Footer />
+            <ConsentBanner
+              settings={{ hidden: true, label: 'More', modal: { title: 'Modal title' } }}
+              decline={{ hidden: false, label: 'Not Accept' }}
+              approve={{ label: 'Accept' }}>
+              <>
+                Can we use cookies and external services according to our <a href="/privacy">privacy policy</a> to
+                improve the browsing experience?
+              </>
+            </ConsentBanner>
+          </ConsentProvider>
         </Providers>
       )}
     </>
